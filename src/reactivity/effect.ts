aa1acfc89ba692ff9792d2d242d06b85503f1f39
lcwang0514 <1332859786@qq.com>
2023-03-06 07:31:48 +0800

    feat:完善effect

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -6,7 +6,7 @@ class ReactiveEffect {
 
   run() {
     activeEffect = this;
-    this._fn();
+    return this._fn();
   }
 }
 /**
@@ -51,4 +51,6 @@ export function effect(fn) {
   // fn
   const _effect = new ReactiveEffect(fn);
   _effect.run();
+
+  return _effect.run.bind(_effect);
 }
