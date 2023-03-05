9c65ff52ba150f9c936954c551343140ce2cae63
lcwang0514 <1332859786@qq.com>
2023-03-05 22:09:02 +0800

    feat:reactive,effect 初始化

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,54 @@
+class ReactiveEffect {
+  private _fn: any;
+  constructor(fn) {
+    this._fn = fn;
+  }
+
+  run() {
+    activeEffect = this;
+    this._fn();
+  }
+}
+/**
+ * 收集依赖
+ * @param target
+ * @param key
+ */
+const targetMap = new Map();
+export function track(target, key) {
+  // target ---> key ---> dep
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
+  }
+
+  let dep = depsMap.get(key);
+  if (!dep) {
+    dep = new Set();
+    depsMap.set(key, dep);
+  }
+
+  dep.add(activeEffect);
+}
+
+/**
+ * 触发依赖
+ * @param target
+ * @param key
+ */
+export function trigger(target, key) {
+  let depsMap = targetMap.get(target);
+  let dep = depsMap.get(key);
+
+  for (const effect of dep) {
+    effect.run();
+  }
+}
+
+let activeEffect;
+export function effect(fn) {
+  // fn
+  const _effect = new ReactiveEffect(fn);
+  _effect.run();
+}
