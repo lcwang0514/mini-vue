54b8016e11ace969ea4ab35be7645981b060eaa9
lcwang0514 <1332859786@qq.com>
2023-03-08 22:42:02 +0800

    feat:实现isReadonly,isReactive

--- src/reactivity/baseHandler.ts
+++ src/reactivity/baseHandler.ts
@@ -1,4 +1,5 @@
 import { track, trigger } from './effect';
+import { ReactiveFlags } from './reactive';
 
 const get = createGetter();
 const set = createSetter();
@@ -6,6 +7,11 @@ const readonlyGet = createGetter(true);
 
 function createGetter(isReadonly = false) {
   return function get(target, key) {
+    if (key === ReactiveFlags.IS_REACTIVE) {
+      return !isReadonly;
+    } else if (key === ReactiveFlags.IS_READONLY) {
+      return isReadonly;
+    }
     const res = Reflect.get(target, key);
     // 依赖收集
     if (!isReadonly) {
@@ -29,7 +35,7 @@ export const mutableHandlers = {
 };
 
 export const readonlyHandlers = {
-  get,
+  get: readonlyGet,
   set(target, key, value) {
     // 给出一些警告
     console.warn(`key: ${key} set failed`);
