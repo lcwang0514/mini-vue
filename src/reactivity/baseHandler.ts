f49a97d270486a4f7dc97f51a2b0f9c6a466f25d
lcwang0514 <1332859786@qq.com>
2023-03-12 11:10:13 +0800

    feat:对象嵌逻辑的实现

--- src/reactivity/baseHandler.ts
+++ src/reactivity/baseHandler.ts
@@ -1,5 +1,6 @@
+import { isObject } from '../shared';
 import { track, trigger } from './effect';
-import { ReactiveFlags } from './reactive';
+import { reactive, ReactiveFlags, readonly } from './reactive';
 
 const get = createGetter();
 const set = createSetter();
@@ -13,6 +14,11 @@ function createGetter(isReadonly = false) {
       return isReadonly;
     }
     const res = Reflect.get(target, key);
+
+    // 如果 res 是 object 继续响应式
+    if (isObject(res)) {
+      return isReadonly ? readonly(res) : reactive(res);
+    }
     // 依赖收集
     if (!isReadonly) {
       track(target, key);
