01ce5953514fe8f3326edafdeed58481ab9d1a05
lcwang0514 <1332859786@qq.com>
2023-03-17 11:03:12 +0800

    feat:添加位运算shapeFlag

--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -1,3 +1,4 @@
+import { ShapeFlags } from '../shared/ShapeFlags';
 import { isObject } from './../shared/index';
 import { createComponentInstance, setupComponent } from './component';
 
@@ -7,12 +8,12 @@ export function render(vnode, container) {
 }
 
 function patch(vnode, container) {
-  if (typeof vnode.type === 'string') {
+  const { shapeFlag } = vnode;
+  if (shapeFlag & ShapeFlags.ELEMENT) {
     processElement(vnode, container);
-  } else if (isObject(vnode.type)) {
+  } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
     processComponent(vnode, container);
   }
-  console.log('vnode.type', vnode.type);
 }
 
 function processComponent(vnode: any, container: any) {
@@ -36,10 +37,10 @@ function processElement(vnode, container) {
 }
 function mountElement(vnode: any, container: any) {
   const el = (vnode.el = document.createElement(vnode.type));
-  const { children } = vnode;
-  if (typeof children === 'string') {
+  const { children, shapeFlag } = vnode;
+  if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
     el.textContent = children;
-  } else if (Array.isArray(children)) {
+  } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
     mountChildren(vnode, el);
   }
 
