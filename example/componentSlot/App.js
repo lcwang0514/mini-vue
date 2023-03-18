9ccfbe0c98b3cd577ca5f54ffd27c7e3da2efe61
lcwang0514 <1332859786@qq.com>
2023-03-18 08:09:17 +0800

    feat:初始化实现组件Fragment和Text节点

--- example/componentSlot/App.js
+++ example/componentSlot/App.js
@@ -1,12 +1,13 @@
-import { h } from '../../lib/guide-mini-vue.esm.js'
+import { h, createTextVNode } from '../../lib/guide-mini-vue.esm.js'
 import { Foo } from './Foo.js'
+
 export const App = {
   name: "App",
   render() {
     const app = h("div", {}, "App")
     const foo = h(Foo, {}, {
       header: ({ age }) => h('p', {}, 'header' + age),
-      footer: () => h('p', {}, 'footer')
+      footer: () => h('p', {}, [createTextVNode('footer')])
     })
     return h("div", {}, [app, foo])
   },
