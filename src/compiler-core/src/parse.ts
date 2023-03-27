08d4679c5ddff7336a11efa4664fac97d566f430
lcwang0514 <1332859786@qq.com>
2023-03-27 21:57:59 +0800

    feat:解析div标签类型

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from './ast';
 
+const enum TagType {
+  Start,
+  End,
+}
+
 export function baseParse(content: string) {
   const context = createParserContext(content);
   return createRoot(parserChildren(context));
@@ -7,13 +12,37 @@ export function baseParse(content: string) {
 
 function parserChildren(context) {
   const nodes: any = [];
+
   let node;
-  if (context.source.startsWith('{{')) {
+  const s = context.source;
+  if (s.startsWith('{{')) {
     node = parseInterpolation(context);
+  } else if (s[0] === '<') {
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context);
+    }
   }
 
   nodes.push(node);
   return nodes;
+}
+
+function parseElement(context) {
+  const element = parseTag(context, TagType.Start);
+  parseTag(context, TagType.End); // 删除 </div>
+  return element;
+}
+
+function parseTag(context: any, type: TagType) {
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+  const tag = match[1];
+  advanceBy(context, match[0].length);
+  advanceBy(context, 1);
+  if (type === TagType.End) return;
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+  };
 }
 
 function parseInterpolation(context) {
