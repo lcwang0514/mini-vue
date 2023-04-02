0f93e2fcdb4edc5214990f33458989670fbbe7c9
lcwang0514 <1332859786@qq.com>
2023-04-02 15:58:39 +0800

    feat:实现插值生成

--- src/compiler-core/src/codegen.ts
+++ src/compiler-core/src/codegen.ts
@@ -1,7 +1,12 @@
+import { TO_DISPLAY_STRING, helpersMapName } from './runtimeHelpers';
+import { NodeTypes } from './ast';
+
 export function generate(ast) {
   const context = createCodegenContext();
   const { push } = context;
-  push('return ');
+
+  // 前导
+  genFunctionPreamble(ast, context);
 
   const functionName = 'render';
   const args = ['_ctx', '_cache'];
@@ -9,30 +14,63 @@ export function generate(ast) {
 
   push(`function ${functionName}(${signature}){`);
   push(`return `);
-  // code += 'return';
   genNode(ast.codegenNode, context);
-  // code = ;
   push('}');
-  // code += '}';
   return {
     code: context.code,
-    // code: `
-    //   return function render(_ctx, _cache, $props, $setup, $data, $options) {
-    //     return 'hi'
-    //   }
-    // `,
   };
 }
 
+function genFunctionPreamble(ast, context) {
+  const { push } = context;
+  const VueBinging = 'Vue';
+  const aliasHelper = (s) => `${helpersMapName[s]}:_${helpersMapName[s]}`;
+  if (ast.helpers.length > 0) {
+    push(`const {${ast.helpers.map(aliasHelper).join(', ')}} = ${VueBinging}`);
+  }
+  push('\n');
+  push('return ');
+}
+
 function genNode(node: any, context) {
+  switch (node.type) {
+    case NodeTypes.TEXT:
+      genText(context, node);
+      break;
+    case NodeTypes.INTERPOLATION:
+      genInterpolation(context, node);
+      break;
+    case NodeTypes.SIMPLE_INTERPOLATION:
+      genExpression(context, node);
+      break;
+    default:
+      break;
+  }
+}
+function genText(context: any, node: any) {
   const { push } = context;
   push(`'${node.content}'`);
 }
+function genInterpolation(context: any, node: any) {
+  const { push, helper } = context;
+  push(`${helper(TO_DISPLAY_STRING)}(`);
+  genNode(node.content, context);
+  push(')');
+}
+
+function genExpression(context: any, node: any) {
+  const { push } = context;
+  push(`${node.content}`);
+}
+
 function createCodegenContext() {
   const context = {
     code: '',
     push(source) {
       context.code += source;
+    },
+    helper(key) {
+      return `_${helpersMapName[key]}`;
     },
   };
   return context;
--- src/compiler-core/tests/codegen.spec.ts
+++ src/compiler-core/tests/codegen.spec.ts
@@ -1,15 +1,25 @@
 import { generate } from '../src/codegen';
 import { baseParse } from '../src/parse';
 import { transform } from '../src/transform';
+import { transformExpression } from '../src/transfroms/transformExpression';
 
 describe('codegen', () => {
-  it.only('string', () => {
+  it('string', () => {
     const ast = baseParse('hi');
     transform(ast);
     const { code } = generate(ast);
     // 快照
     //1 抓 bug
     // 2 有意更新
+    expect(code).toMatchSnapshot();
+  });
+
+  it('interpolation', () => {
+    const ast = baseParse('{{message}}');
+    transform(ast, {
+      nodeTransforms: [transformExpression],
+    });
+    const { code } = generate(ast);
     expect(code).toMatchSnapshot();
   });
 });
